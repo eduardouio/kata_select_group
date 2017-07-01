5fdf126a50d64b240286dd7785aac03edc022cc2
Eduardo Villota <eduardouio7@gmail.com>
2017-07-01 15:03:11 -0500

    Paso de los dos TDDS, funcionando solo la primera -_-

--- client/app/scripts/main/controller.js
+++ client/app/scripts/main/controller.js
@@ -5,4 +5,53 @@ angular.module('App')
 
   $scope.controller_loaded = 'Main controller loaded!';
 
+  $scope.sum = function(numa, numb){
+    //console.log('[Deb] Hola');
+    var sum = (numa + numb );
+    return sum;
+
+};
+
+  $scope.validateForm =  function(employedEst, employedLon) {
+    //para pareja de usuarios
+    if(employedEst.length > 0){
+      $scope.result = [];
+      if (employedEst[0] < employedLon[0]){
+        $scope.result.push(employedEst[0]);
+      }else{
+        $scope.result.push(employedLon[0]);
+      }
+    }
+
+    //para dos empleados
+    //expect(scope.validateForm([1009, 1017], [2011,2011])).toEqual([2011]);
+    if(employedEst.length > 1){
+      //Personas repetidas en estocolmo
+      var repeatedEst = [];
+      var repeatedLon = [];
+      $scope.result = [];
+         for (var i = 0; i < employedEst.length; i++) {
+            for (var emp = 0; emp < employedEst.length; emp++) {
+              if (employedEst[i] === employedEst[emp]){
+                repeatedEst.push(employedEst[emp]);
+              }
+            }
+         }
+
+         //empleador Londres
+         for (var i = 0; i < employedLon.length; i++) {
+           for (var emp = 0; emp < employedLon.length; emp++) {
+             if (employedLon[i] === employedLon[emp]){
+               repeatedLon.push(employedLon(emp));
+             }
+           }
+         }
+
+    }
+
+
+    console.log('[#30] ' + $scope.result);
+    return $scope.result;
+
+  };
 });
--- client/app/scripts/main/specs/controller_spec.js
+++ client/app/scripts/main/specs/controller_spec.js
@@ -12,9 +12,35 @@ describe('Controller: Main', function () {
     controller = $controller('Main', { $scope: scope });
   }));
 
+  /**
+   * TDD para funcion suma
+   */
   describe('On instance', function(){
     it('should set "controller_loaded" variable in scope', function() {
       expect(scope.controller_loaded).toContain('loaded');
+    });
+
+    it('suma de dos numeros', function(){
+      expect(scope.sum(2, 2)).toEqual(4);
+      expect(scope.sum(4, 19)).toEqual(23);
+    });
+
+    /**
+     * selecion 2 usuarios
+     */
+    it('registro de una parejas', function () {
+      expect(scope.validateForm([2001], [1003])).toEqual([1003]);
+    });
+
+    /**
+     * Selecion para mas de 2 parejas
+     */
+    it('registro de varia parejas', function () {
+      expect(scope.validateForm([1009, 1017], [2011,2011])).toEqual([2011]);
+    });
+
+    it('registro de varia parejas', function () {
+      expect(scope.validateForm([1009, 1017,1016,1432], [2005,2005,2005,2005])).toEqual([2005]);
     });
   });
 
